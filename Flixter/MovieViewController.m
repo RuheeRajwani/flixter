560ce507ffcefda4ab410fce630d86ab5bfbd961
Ruhee Rajwani <ruheer@fb.com>
2022-06-15 16:15:08 -0700

    working stage 2

---
+++ Flixter/MovieViewController.m
@@ -0,0 +1,53 @@
+//
+//  MovieViewController.m
+//  Flixter
+//
+//  Created by Ruhee Rajwani on 6/15/22.
+//
+
+#import "MovieViewController.h"
+
+@interface MovieViewController ()
+
+@property (nonatomic, strong) NSArray *movieArray;
+
+@end
+
+@implementation MovieViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=3936a31f61adbb8965840fb14ad1ca82"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+               // TODO: Get the array of movies
+               NSLog(@"%@", dataDictionary);
+               // TODO: Store the movies in a property to use elsewhere
+               NSArray *movieArray = dataDictionary[@"keyForAnArray"];
+               // TODO: Reload your table view data
+//               [self.tableView reloadData];
+           }
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
