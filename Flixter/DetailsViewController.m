37ce3bd36bd4175630fa06a1183eef76641bf30e
Ruhee Rajwani <ruheer@fb.com>
2022-06-17 14:31:38 -0700

    working stage 6

--- Flixter/DetailsViewController.m
+++ Flixter/DetailsViewController.m
@@ -20,15 +20,14 @@
 
 @implementation DetailsViewController
 
+//ADD COMMENT
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     NSLog(@"%@", self.detailDict);
     self.detailMovieTitle.text = self.detailDict[@"title"];
     self.detailMovieSynopsis.text = self.detailDict[@"overview"];
     self.detailMoviePopularityValue.text = [NSString stringWithFormat:@"%@", self.detailDict[@"popularity"]];
     self.detailMovieReleaseDateValue.text = self.detailDict[@"release_date"];
-    
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = self.detailDict[@"poster_path"];
     NSString *fullPosterURLString =[baseURLString stringByAppendingString:posterURLString];
@@ -36,17 +35,6 @@
     self.detailMovieImage.image = nil;
     [self.detailMovieImage setImageWithURL:posterURL];
     
-    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -22,7 +22,7 @@
 @implementation MovieViewController
 
 
-
+//ADD COMMENT
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.tableView.dataSource = self;
@@ -34,6 +34,7 @@
     
 }
 
+//ADD COMMENT
 - (void)fetchMovies{
     [self.activityIndicator startAnimating];
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=3936a31f61adbb8965840fb14ad1ca82"];
@@ -41,16 +42,20 @@
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
+               //PUT THIS IN ANOTHER FUNCTION
                NSLog(@"%@", [error localizedDescription]);
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline" preferredStyle:UIAlertControllerStyleAlert];
+
+               UIAlertAction *tryAgain = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                   [self fetchMovies];
+                                   }];
+               [alert addAction:tryAgain];
+               [self presentViewController:alert animated:YES completion:nil];
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-
-               // TODO: Get the array of movies
                NSLog(@"%@", dataDictionary);
-               // TODO: Store the movies in a property to use elsewhere
                self.movieArray = dataDictionary[@"results"];
-               // TODO: Reload your table view data
                [self.tableView reloadData];
            }
         [self.refreshControl endRefreshing];
@@ -72,8 +77,6 @@
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     MovieViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieViewCell" forIndexPath:indexPath];
-
-    
     NSDictionary *movie = self.movieArray[indexPath.row];
     cell.titleLabel.text = movie[@"title"];
     cell.synopsisLabel.text = movie[@"overview"];
